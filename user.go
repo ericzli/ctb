7008f8f553d095b9b5d0220c6a82c91a88175408
ericzli <66206606@qq.com>
2021-02-20 17:52:49 +0800

    引入vue来简化前端展示的逻辑

--- user.go
+++ user.go
@@ -10,20 +10,26 @@ func handleRegisterOrLogin(w http.ResponseWriter, r *http.Request) {
 	defer errRecover4Rest(w)
 
 	user := r.FormValue("user")
+	user_id := 0
+	row := s_DB.QueryRow("select id from ctb_user where user = ?", user)
+	err := row.Scan(&user_id)
+	if err == nil && user_id > 0 {
+		w.Write([]byte(`{"result":"ok"}`))
+		return
+	}
+
 	match, err := regexp.MatchString(`[a-zA-Z0-9]+`, user)
 	if !match || err != nil {
 		fmt.Printf("Invalid user name %s, err = %v\n", user, err)
 	}
 
-	result, err := s_DB.Exec("insert INTO ctb_user(user) select ? where not exists (select user from ctb_user where user = ?)",
-		user, user)
+	result, err := s_DB.Exec("insert INTO ctb_user(user) values(?)", user)
 	checkf(err, "add user failed")
 	lastInsertID, err := result.LastInsertId()
-	if err == nil {
-		fmt.Printf("Add new user %s, id %d\n", user, lastInsertID)
-	}
+	checkf(err, "add user get id failed")
 
 	w.Write([]byte(`{"result":"ok"}`))
+	fmt.Printf("Add new user %s, id %d\n", user, lastInsertID)
 }
 
 func getUserId(r *http.Request) int {
